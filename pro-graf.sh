322c6f9eb8325512a5a0fe29fcb880b4a4b26943
Maha <67692329+M404h@users.noreply.github.com>
2020-09-01 15:16:20 +0300

    Update pro-graf.sh
    added toleration file as values

--- pro-graf.sh
+++ pro-graf.sh
@@ -2,5 +2,5 @@
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
 kubectl create namespace monitor
-helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer
+helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer --values toleration.yaml
 kubectl apply -f ingress.yaml -n monitor
